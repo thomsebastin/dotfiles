d43a562467e90317703e7880996dbec5b7eb4139
Thomas Sebastian <thomas.jsm13@gmail.com>
2018-07-08 16:01:31 +0530

    added helm config files

--- init.el
+++ init.el
@@ -57,13 +57,62 @@
             ("C-c C-<" . mc/mark-all-like-this)
 ))
 
-;; enable projectile mode
+;; helm related configs
+(use-package helm
+  :ensure t
+  :bind (("M-a" . helm-M-x)
+         ("C-x C-f" . helm-find-files)
+         ("C-x f" . helm-recentf)
+         ("C-SPC" . helm-dabbrev)
+         ("M-y" . helm-show-kill-ring)
+         ("C-x b" . helm-buffers-list))
+  :bind (:map helm-map
+	      ("M-i" . helm-previous-line)
+	      ("M-k" . helm-next-line)
+	      ("M-I" . helm-previous-page)
+	      ("M-K" . helm-next-page)
+	      ("M-h" . helm-beginning-of-buffer)
+	      ("M-H" . helm-end-of-buffer))
+  :config (progn
+	    (setq helm-buffers-fuzzy-matching t)
+            (helm-mode 1)))
+(use-package helm-descbinds
+  :ensure t
+  :bind ("C-h b" . helm-descbinds))
+(use-package helm-files
+  :bind (:map helm-find-files-map
+	      ("M-i" . nil)
+	      ("M-k" . nil)
+	      ("M-I" . nil)
+	      ("M-K" . nil)
+	      ("M-h" . nil)
+	      ("M-H" . nil)))
+(use-package helm-swoop
+  :ensure t
+  :bind (("M-m" . helm-swoop)
+	 ("M-M" . helm-swoop-back-to-last-point))
+  :init
+  (bind-key "M-m" 'helm-swoop-from-isearch isearch-mode-map))
+(use-package helm-ag
+  :ensure helm-ag
+  :bind ("M-p" . helm-projectile-ag)
+  :commands (helm-ag helm-projectile-ag)
+  :init (setq helm-ag-insert-at-point 'symbol
+	      helm-ag-command-option "--path-to-ignore ~/.agignore"))
+
 (use-package projectile
-  :ensure  projectile
+  :ensure t
+  :bind (("C-p s" . projectile-switch-open-project)
+	 ("C-x p" . projectile-switch-project))
   :config
-    (projectile-global-mode t)
-    (setq projectile-enable-caching t)
-    (setq projectile-indexing-method 'native))
+  (projectile-global-mode)
+  (setq projectile-enable-caching t))
+
+(use-package helm-projectile
+  :ensure t
+  :bind ("M-t" . helm-projectile-find-file)
+  :config
+  (helm-projectile-on))
 
 ;; neotree
 (use-package neotree
